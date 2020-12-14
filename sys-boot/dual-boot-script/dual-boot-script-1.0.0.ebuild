e18996480d19a949f1898dd16d0093896f61ead0
Bradley Eaton <mail.thebradad1111@gmail.com>
2020-12-14 16:34:42 +0000

    Dual-boot script

---
+++ sys-boot/dual-boot-script/dual-boot-script-1.0.0.ebuild
@@ -0,0 +1,29 @@
+# Copyright 2020 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+PYTHON_COMPAT=( python3_{7,8,9} )
+inherit python-single-r1
+
+DESCRIPTION="Script that boots to Windows"
+HOMEPAGE="https://gist.github.com/Darkhogg/82a651f40f835196df3b1bd1362f5b8c"
+SRC_URI=""
+
+LICENSE=""
+SLOT="0"
+KEYWORDS="~amd64 ~x86"
+IUSE=""
+REQUIRED_USE="${PYTHON_REQUIRED_USE}"
+
+#DEPEND=""
+RDEPEND="${PYTHON_DEPS}
+         sys-fs/e2fsprogs
+"
+BDEPEND=""
+
+S="${FILESDIR}"
+
+src_install(){
+	python_newscript boot-windows.py boot-windows
+}
---
+++ sys-boot/dual-boot-script/files/boot-windows.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Modified from https://gist.github.com/Darkhogg/82a651f40f835196df3b1bd1362f5b8c
+import subprocess
+import sys
+
+EFIVAR_NAME = 'PreviousBoot'
+EFIVAR_GUID = '36d08fa7-cf0b-42f5-8f14-68df73ed3740'
+EFIVAR_PREFIX = '/sys/firmware/efi/efivars'
+
+PREFIX = b'\x07\x00\x00\x00'
+SUFFIX = b'\x20\x00\x00\x00'
+
+text = "Microsoft" 
+filename = '{}/{}-{}'.format(EFIVAR_PREFIX, EFIVAR_NAME, EFIVAR_GUID)
+
+retcode = subprocess.call(['chattr', '-i', filename])
+if retcode != 0:
+    sys.exit(42 + retcode)
+
+with open(filename, 'wb') as f:
+    content = PREFIX + bytes(text, 'utf-16-le') + SUFFIX
+    f.write(content)
