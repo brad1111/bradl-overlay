e8c08d3b7d75f648dc20380c12ae35bb5f6e043e
Bradley Eaton <bradley@bradleyeaton.com>
2021-05-17 23:51:25 +0100

    Updated dual boot script

--- sys-boot/dual-boot-script/files/boot-windows.py
+++ sys-boot/dual-boot-script/files/boot-windows.py
@@ -3,20 +3,25 @@
 import subprocess
 import sys
 
-EFIVAR_NAME = 'PreviousBoot'
-EFIVAR_GUID = '36d08fa7-cf0b-42f5-8f14-68df73ed3740'
-EFIVAR_PREFIX = '/sys/firmware/efi/efivars'
-
-PREFIX = b'\x07\x00\x00\x00'
-SUFFIX = b'\x20\x00\x00\x00'
-
+# For efivars
+# EFIVAR_NAME = 'PreviousBoot'
+# EFIVAR_GUID = '36d08fa7-cf0b-42f5-8f14-68df73ed3740'
+# EFIVAR_PREFIX = '/sys/firmware/efi/efivars'
+# 
+# PREFIX = b'\x07\x00\x00\x00'
+ SUFFIX = b'\x20\x00\x00\x00'
+# 
 text = "Microsoft" 
-filename = '{}/{}-{}'.format(EFIVAR_PREFIX, EFIVAR_NAME, EFIVAR_GUID)
+# filename = '{}/{}-{}'.format(EFIVAR_PREFIX, EFIVAR_NAME, EFIVAR_GUID)
+# 
+# retcode = subprocess.call(['chattr', '-i', filename])
+# if retcode != 0:
+#     sys.exit(42 + retcode)
+# 
 
-retcode = subprocess.call(['chattr', '-i', filename])
-if retcode != 0:
-    sys.exit(42 + retcode)
+
+filename = '/boot/EFI/refind/vars/PreviousBoot'
 
 with open(filename, 'wb') as f:
-    content = PREFIX + bytes(text, 'utf-16-le') + SUFFIX
+    content = bytes(text, 'utf-16-le') + SUFFIX
     f.write(content)
